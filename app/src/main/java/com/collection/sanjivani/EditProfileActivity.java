ba818f801986f47c501c6620ee8464e2cbb635d3
harik047sharma <hardik.sharmakkll@gmail.com>
2020-05-16 13:45:16 +0530

    thoda bhout sahi krdiya

--- app/src/main/java/com/collection/sanjivani/EditProfileActivity.java
+++ app/src/main/java/com/collection/sanjivani/EditProfileActivity.java
@@ -10,5 +10,6 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
